5444ecb6cb0149e090dbfb83eccab88810a4678f
a0111770 <tanteckli@gmail.com>
2014-10-17 08:16:58 +0800

    remove UICALL system.out

--- FlexiPlanner/src/Logic/UICall.java
+++ FlexiPlanner/src/Logic/UICall.java
@@ -22,7 +22,6 @@ public class UICall {
 
 	public String getData(String s) {		
 		if (s.toLowerCase().startsWith("search")) {
-			System.out.println("haha");
 			return logic.searchRes;
 		}
 		return logic.dataToShow();
