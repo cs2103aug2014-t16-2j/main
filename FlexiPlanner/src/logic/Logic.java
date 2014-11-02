8db709438e1a233959e1e71fb7d79cd67e06c57e
Moe <hein.moelwin@gmail.com>
2014-11-02 20:45:45 +0800

    Added reminder add methods to logic AGAIN

--- FlexiPlanner/src/logic/Logic.java
+++ FlexiPlanner/src/logic/Logic.java
@@ -13,8 +13,8 @@ import java.util.Stack;
 import org.json.simple.parser.ParseException;
 
 import commons.TaskData;
-
 import parser.*;
+import reminder.ReminderPatternParser;
 import storage.*;
 
 public class Logic {
@@ -39,6 +39,11 @@ public class Logic {
 	boolean isSuspendedAction = false;
 	Action suspendingAction;
 	ArrayList<TaskData> F3DisplayedList;
+	
+	private ReminderPatternParser reminderParser;
+	
+	private LocalDateTime reminderDateTime;
+	private Integer reminderMinutes;
 
 	int currentDisplayList;
 	static Scanner sc = new Scanner(System.in);
@@ -69,6 +74,7 @@ public class Logic {
 		parser = new Parser();
 		searchTool = new SearchTool();
 		entry = new ActionEntry(action, null);
+		reminderParser = new ReminderPatternParser();
 		loadData();
 	}
 
@@ -145,6 +151,39 @@ public class Logic {
 			command = suspendingAction.getCommand();
 			Task t = suspendingAction.getTask();
 			task.setContent(t.getContent());
+		}
+		getReminderDateTime(_command, toTaskData(task)); // get reminder date/time
+	}
+	
+	/**
+	 * get reminder date and time from reminder parser
+	 */
+	private void getReminderDateTime(String command, TaskData t) {
+		Object obj = reminderParser.parse(command);
+		
+		if (obj == null) {
+			reminderDateTime = null;
+			reminderMinutes = null;
+		}
+		else if (obj instanceof LocalDateTime) {
+			reminderDateTime = (LocalDateTime) obj;
+		}
+		else if (obj instanceof Integer) {
+			reminderMinutes = (Integer) obj;
+			
+			if ((t.getStartDateTime() == null) &&
+					(t.getEndDateTime() == null)) {
+
+				reminderDateTime = null;
+			}
+			else if (t.getStartDateTime() != null) {
+				reminderDateTime =
+						t.getStartDateTime().minusMinutes(reminderMinutes);
+			}
+			else if (t.getEndDateTime() != null) {
+				reminderDateTime =
+						t.getEndDateTime().minusMinutes(reminderMinutes);
+			}
 		}
 	}
 
@@ -231,6 +270,19 @@ public class Logic {
 			map.put(new DateInfo(task.getStartDateTime(), task.getEndDateTime()),
 					task);
 		}
+		
+		/** set reminder **/
+
+		if (reminderDateTime != null) {
+			task.setRemindDateTime(reminderDateTime);
+			task.setReminder();
+		}
+		else {
+			System.out.println("Reminder date and time is not set!");
+		}
+		
+		/** **/
+		
 		taskList.add(task);
 		F2DisplayedList.add(0, task);
 		try {
@@ -306,6 +358,7 @@ public class Logic {
 							toDelete.getEndDateTime());
 					toDeleteList.remove(d);
 				}
+				toDelete.clearReminder(); // to kill the background reminder app
 				taskList.remove(toDelete);
 				if (F2DisplayedList.contains(toDelete)) {
 					F2DisplayedList.remove(toDelete);
